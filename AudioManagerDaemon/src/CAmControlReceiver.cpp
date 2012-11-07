6fc5e30f3334b4601706d0d411c1a8b15090880c
Goehring <Thomas.Goehring@continental-corporation.com>
2012-11-07 14:54:44 +0100

    Genivi AudioManager Bug 13: error on update MainSourceSoundProperty in daemon DB
    Signed-off-by: Goehring <Thomas.Goehring@continental-corporation.com>

--- AudioManagerDaemon/src/CAmControlReceiver.cpp
+++ AudioManagerDaemon/src/CAmControlReceiver.cpp
@@ -289,7 +289,7 @@ am_Error_e CAmControlReceiver::changeMainSinkSoundPropertyDB(const am_MainSoundP
 
 am_Error_e CAmControlReceiver::changeMainSourceSoundPropertyDB(const am_MainSoundProperty_s & soundProperty, const am_sourceID_t sourceID)
 {
-    return (mDatabaseHandler->changeMainSinkSoundPropertyDB(soundProperty, sourceID));
+    return (mDatabaseHandler->changeMainSourceSoundPropertyDB(soundProperty, sourceID));
 }
 
 am_Error_e CAmControlReceiver::changeSourceAvailabilityDB(const am_Availability_s & availability, const am_sourceID_t sourceID)
